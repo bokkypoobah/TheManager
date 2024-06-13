58548f98661ea8d76c71e4181ed80db58ffbbbe2
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-06-13 21:30:29 +1000

    Free Pertsev!

--- docs/nonFungibleTokens.js
+++ docs/nonFungibleTokens.js
@@ -172,7 +172,7 @@ const NonFungibleTokens = {
             <!-- {{ data.item.attributes }} -->
             <b-row v-for="(attribute, i) in data.item.attributes"  v-bind:key="i" class="m-0 p-0">
               <b-col cols="3" class="m-0 px-2 text-right"><font size="-3">{{ attribute.trait_type }}</font></b-col>
-              <b-col cols="9" class="m-0 px-2"><b><font size="-2">{{ ["Created Date", "Registration Date", "Expiration Date"].includes(attribute.trait_type) ? formatTimestamp(attribute.value) : attribute.value }}</font></b></b-col>
+              <b-col cols="9" class="m-0 px-2"><b><font size="-2">{{ ["Created", "Registration", "Expiry"].includes(attribute.trait_type) ? formatTimestamp(attribute.value) : attribute.value }}</font></b></b-col>
             </b-row>
           </template>
 

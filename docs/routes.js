0ddcfbebf63c21715454eea1bbbba62e876a7d47
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-06-21 01:49:29 +1000

    Free Pertsev!

--- docs/routes.js
+++ docs/routes.js
@@ -2,55 +2,14 @@ const routes = [{
     path: '/config',
     component: Config,
     name: 'Config',
-  // }, {
-  //   path: '/mappings',
-  //   component: Mappings,
-  //   name: 'Mappings',
-  // }, {
-  //   path: '/account',
-  //   component: Account,
-  //   name: 'Account',
   }, {
     path: '/names',
     component: Names,
     name: 'Names',
-  // }, {
-  //   path: '/fungibleTokens',
-  //   component: FungibleTokens,
-  //   name: 'FungibleTokens',
-  // }, {
-  //   path: '/stealthtransfers',
-  //   component: StealthTransfers,
-  //   name: 'StealthTransfers',
-  // }, {
-  //   path: '/registry',
-  //   component: Registry,
-  //   name: 'Registry',
   }, {
     path: '/addresses',
     component: Addresses,
     name: 'Addresses',
-  // }, {
-  //   path: '/assets',
-  //   component: Assets,
-  //   name: 'Assets',
-  // }, {
-  //   path: '/report/:contractOrTxOrBlockRange?',
-  //   component: Report,
-  //   name: 'Report',
-  //   props: true,
-  // }, {
-  //   path: '/names',
-  //   component: Names,
-  //   name: 'Names',
-  // }, {
-  //   path: '/data',
-  //   component: Data,
-  //   name: 'Data',
-  // }, {
-  //   path: '/docs/:section/:topic',
-  //   component: Docs,
-  //   name: 'Docs',
   }, {
     path: '*',
     component: Welcome,
